1a0958b175ee3c59aebcf491ffd7a797db0c2d6a
josepg2 <georgejoseph2sep92@gmail.com>
2017-03-31 00:50:33 +0530

    Added marquee

--- main.js
+++ main.js
@@ -12,48 +12,45 @@ const url = require('url')
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
 
-  //mainWindow.setFullScreen(true)
+    //mainWindow.setFullScreen(true)
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+    // and load the index.html of the app.
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
-	
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools()
 
 
-  setTimeout(function() {
-var manager = new OmxManager(); // OmxManager 
-manager.setVideosDirectory('/home/pi/Electron/DisplayServer/Videos');
-  var camera = manager.create(['small.mp4', 'small2.mp4'],
- {
- '--loop': true,
- '-o': 'hdmi',
- '--win' : "1330 540 1920 1080"
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null
+    })
 
-}
 
-);
-  camera.play();
-
-	}, 3000)
-  
+    //setTimeout(function() {
+    //    var manager = new OmxManager(); // OmxManager
+    //    manager.setVideosDirectory('/home/pi/Electron/DisplayServer/Videos');
+    //    var camera = manager.create(['small.mp4', 'small2.mp4'], {
+    //        '--loop': true,
+    //        '-o': 'hdmi',
+    //        '--win': "1330 540 1920 1080"
+    //    });
+    //    camera.play();
+    //}, 3000)
 };
 
 // This method will be called when Electron has finished
@@ -62,20 +59,20 @@ manager.setVideosDirectory('/home/pi/Electron/DisplayServer/Videos');
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+app.on('window-all-closed', function() {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
+app.on('activate', function() {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
 })
 
 // In this file you can include the rest of your app's specific main process
